8e45f2d9115c147ac48a1924a4b5629baf773eb3
YoyokWidi <yoayoe@gmail.com>
2020-06-25 22:22:47 +0900

    Menambahkan database Organisasi

--- network_automation/admin.py
+++ network_automation/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Device
+from network_automation.models import Device, Organisasi
 
+class ListDevice(admin.ModelAdmin):
+    list_display = ['hostname','lokasi_unit', 'ip_address', 'vendor']
+    search_fields = ['lokasi_unit', 'ip_address', 'hostname', 'vendor']
+    list_per_page = 5
 
-admin.site.register(Device)
+admin.site.register(Device, ListDevice)
+admin.site.register(Organisasi)
--- network_automation/models.py
+++ network_automation/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 from datetime import datetime
 
+class Organisasi(models.Model):
+    LIST_UNIT_INDUK = (
+        ('uiw mmu', 'UIW Maluku dan Maluku Utara'),
+        ('uip maluku', 'UIP Maluku'),
+    )
+    unit_induk = models.CharField(max_length=255,choices=LIST_UNIT_INDUK)
+    LIST_UNIT_PELAKSANA = (
+        ('UIW Maluku dan Maluku Utara', 'UIW Maluku dan Maluku Utara'),
+        ('UIP Maluku', 'UIP Maluku'),
+        ('UP3 Ambon', 'UP3 Ambon'),
+        ('UP3 Masohi', 'UP3 Masohi'),
+        ('UP3 Tual', 'UP3 Tual'),
+        ('UP3 Saumlaki', 'UP3 Saumlaki'),
+        ('UP3 Ternate', 'UP3 Ternate'),
+        ('UP3 Sofifi', 'UP3 Sofifi'),
+        ('UP3 Tobelo', 'UP3 Tobelo'),
+        ('UPK Maluku', 'UPK Maluku'),
+        ('UPP Maluku', 'UPP Maluku'),
+        ('UPP Maluku Utara', 'UPP Maluku Utara'),
+    )
+    unit_pelaksana = models.CharField(null=True,max_length=255,choices=LIST_UNIT_PELAKSANA)
+    unit_layanan = models.CharField(null=True, max_length=255)
 
+    def __str__(self):
+        return "{}. {}. {}".format(self.unit_induk, self.unit_pelaksana, self.unit_layanan)
+    
 class Device(models.Model):
     ip_address = models.CharField(max_length=255)
     hostname = models.CharField(max_length=255)
@@ -11,12 +36,15 @@ class Device(models.Model):
 
     VENDOR_CHOICES = (
         ('mikrotik', 'Mikrotik'),
-        ('cisco', 'Cisco')
+        ('cisco', 'Cisco'),
     )
     vendor = models.CharField(max_length=255, choices=VENDOR_CHOICES)
+    koordinat = models.CharField(null=True, max_length=255)
+    lokasi_unit = models.ForeignKey(Organisasi, null=True, on_delete=models.CASCADE)
+    unit = models.CharField(null=True, max_length=255)
 
     def __str__(self):
-        return "{}. {}. {}".format(self.id, self.ip_address, self.hostname)
+        return "{}. {}. {}".format(self.ip_address, self.hostname, self.unit)
 
 class Log(models.Model):
     target = models.CharField(max_length=255)
